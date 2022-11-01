1c9a0d6ab54d29be60a49d23007a3568e6fe90e5
David Earley <david.earley2@gmail.com>
2022-11-01 11:18:02 -0400

    Solved first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (!(num >=0)) return 'ERROR';
+
+    let stringConcat = '';
+
+    for (let i=0; i<num; i++){
+        stringConcat += string;
+    }
+    return stringConcat;
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let myArray = string.split("");
+    let newArray = [];
+
+
+    for (i in myArray){
+        let newIndex = myArray.length - 1 - i;
+        newArray[newIndex] = myArray[i];
+    }
+
+    return myArray.toString();
 };
 
 // Do not edit below this line
